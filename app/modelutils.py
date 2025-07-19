7848ca3c7ebf82b46266a105dcd95c17e222ed1a
lokesh_s_2k6 <lokesh_s_2k6@gmail.com>
2025-07-19 14:55:27 +0530

    ctc losses added and model has been compiled before loading weights

--- app/modelutils.py
+++ app/modelutils.py
@@ -2,6 +2,16 @@ import os
 from tensorflow.keras.models import Sequential 
 from tensorflow.keras.layers import Conv3D, LSTM, Dense, Dropout, Bidirectional, MaxPool3D, Activation, Reshape, SpatialDropout3D, BatchNormalization, TimeDistributed, Flatten
 
+def CTCLoss(y_true, y_pred):
+    batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
+    input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
+    label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
+
+    input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+    label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+
+    loss = tf.keras.backend.ctc_batch_cost(y_true, y_pred, input_length, label_length)
+    return loss
 
 def load_model() -> Sequential: 
     model = Sequential()
@@ -27,6 +37,8 @@ def load_model() -> Sequential:
     model.add(Dropout(.5))
 
     model.add(Dense(41, kernel_initializer='he_normal', activation='softmax'))
+
+    model.compile(optimizer=Adam(learning_rate=0.0001), loss=CTCLoss)
 
     model.load_weights('app/model_lip_reading.h5')
 
